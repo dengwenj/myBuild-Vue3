360177389c1265fae3c4db6710921e491dfbbfc2
dengwenj <927561237@qq.com>
2022-03-24 17:21:18 +0800

    配置webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,33 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { DefinePlugin } = require('webpack')
 
 module.exports = {
   mode: 'development',
-  devtool: 'source-map',
+  devtool: 'cheap-module-source-map',
   entry: './src/index.ts',
   output: {
     filename: 'js/bunild.js',
     path: path.resolve(__dirname, 'dest'),
     publicPath: '/'
   },
+  resolve: {
+    extensions: ['.js', '.ts', '.json', '.wasm', '.jsx', '.tsx', '.vue'],
+    // 别名
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
   devServer: {
     hot: 'only',
     hot: true,
+    port: 2217,
+    open: false,
+    compress: true, // 开启 gzip ，性能压缩
+    historyApiFallback: true,
   },
+
   module: {
     rules: [
       {
@@ -29,7 +42,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.ts$/,
+        test: /\.(j|t)s$/,
         exclude: /node_modules/,
         use: [
           // 'ts-loader',
@@ -49,25 +62,29 @@ module.exports = {
       }
     ]
   },
+
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'dengnwj',
       template: './public/index.html'
     }),
-    // new CopyWebpackPlugin({
-    //   patterns: [
-    //     {
-    //       from: 'public',
-    //       globOptions: {
-    //          // 要忽略的文件
-    //         ignore: [
-    //           '**/index.html'
-    //         ]
-    //       }
-    //     }
-    //   ]
-    // }),
-    new VueLoaderPlugin()
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+             // 要忽略的文件
+            ignore: [
+              '**/index.html'
+            ]
+          }
+        }
+      ]
+    }),
+    new VueLoaderPlugin(),
+    new DefinePlugin({
+      BASE_URL: '"./"'
+    })
   ]
 }
